eb3e67f77d3397541115fe131885346a50dac098
Michał Kuźba <kmichael00968@gmail.com>
2018-11-08 17:39:25 +0100

    add initial version of cp plot

---
+++ ceteris_paribus/profiles.py
@@ -0,0 +1,75 @@
+import re
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def individual_variable_profile(model, data, names, new_observation, y=None, selected_variables=None,
+                                predict_function=None,
+                                grid_points=21, label=None):
+    """
+    Single Ceteris Paribus profile
+    TODO document this
+    :param model:
+    :param data:
+    :param names:
+    :param new_observation:
+    :param y:
+    :param selected_variables:
+    :param predict_function:
+    :param grid_points:
+    :param label:
+    :return:
+    """
+    if not predict_function:
+        predict_function = model.predict
+    if not label:
+        label = re.split('\(', model.__str__())[0]
+    if selected_variables:
+        if not selected_variables in names:
+            raise ValueError('Invalid variable names')
+    else:
+        selected_variables = names
+
+    variables_dict = dict(zip(names, data.T))
+
+    chosen_variables_dict = dict((var, variables_dict[var]) for var in selected_variables)
+
+    cp_dict = {}
+
+    variable_splits = calculate_variable_splits(chosen_variables_dict, grid_points)
+
+    result = list()
+
+    for var, X_var in variable_splits.items():
+        cp_dict[var] = np.tile(new_observation, (grid_points, 1))
+        var_index = names.index(var)
+        cp_dict[var][::, var_index] = X_var
+        X_cp = cp_dict[var]
+        y_cp = predict_function(X_cp)
+        result.append((variable_splits[var], y_cp, var))
+
+    return result
+
+
+def plot(cp_profile):
+    f, axarr = plt.subplots(1, len(cp_profile))
+    for i, (X, y, var) in enumerate(cp_profile):
+        axarr[i].plot(X, y, '-bo')
+        axarr[i].set_title(var)
+    plt.show()
+
+
+def calculate_variable_splits(chosen_variables_dict, grid_points):
+    return dict((var, _calculate_single_split(X_var, grid_points)) for (var, X_var) in chosen_variables_dict.items())
+
+
+def _calculate_single_split(X_var, grid_points):
+    """
+    TODO works for numeric only
+    :param X_var:
+    :param grid_points:
+    :return:
+    """
+    quantiles = np.linspace(0, 1, grid_points)
+    return np.quantile(X_var, quantiles)
---
+++ example.py
@@ -0,0 +1,29 @@
+from sklearn import datasets, linear_model
+
+from ceteris_paribus.profiles import individual_variable_profile, plot
+
+diabetes = datasets.load_diabetes()
+diabetes_X = diabetes.data[:, :5]
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+# Split the targets into training/testing sets
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+
+
+def linear_regression():
+    # Create linear regression object
+    lr_model = linear_model.LinearRegression()
+
+    # Train the model using the training sets
+
+    lr_model.fit(diabetes_X_train, diabetes_y_train)
+
+    # data labels model names
+    return diabetes_X_train, diabetes_y_train, lr_model, ['col1', 'col2', 'col3', 'col4', 'col5']
+
+
+if __name__ == "__main__":
+    (data, labels, model, names) = linear_regression()
+    cp_profile = individual_variable_profile(model, data, names, diabetes_X_test[1])
+    plot(cp_profile)
