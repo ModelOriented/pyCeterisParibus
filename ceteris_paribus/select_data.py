926e702ab99bc3db1e37d04849d2079be7266ce1
Michał Kuźba <kmichael00968@gmail.com>
2018-11-29 16:22:53 +0100

    update data selection

--- ceteris_paribus/select_data.py
+++ ceteris_paribus/select_data.py
@@ -1,12 +1,47 @@
+import logging
+
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
 
 
-def select_sample(data, n):
-    return data[np.random.choice(data.shape[0], n, replace=False), :]
+def select_sample(data, y=None, n=15, seed=42):
+    """
+    Select sample from dataset.
+    :param data: array with observations
+    :param y: labels for observations
+    :param n: size of the sample
+    :param seed: seed for random number generator
+    :return: selected observations and corresponding labels if provided
+    """
+    np.random.seed(seed)
+    if n > data.shape[0]:
+        logging.warning("Given n ({}) is larger than data size ({})".format(n, data.shape[0]))
+        n = data.shape[0]
+    indices = np.random.choice(data.shape[0], n, replace=False)
+    if y is not None:
+        return data[indices, :], y[indices]
+    else:
+        return data[indices, :]
 
 
-def select_neighbours(data, observation, variables=None, distance=euclidean_distances, n=20, frac=None):
-    distances = euclidean_distances([observation], data)[0]
-    selected_points = data[np.argpartition(distances, n)[:n], :]
-    return selected_points
+def select_neighbours(data, observation, y=None, variables=None, distance=euclidean_distances, n=20):
+    """
+    Select observations from dataset, that are similar to a given observation
+    :param data: array with observations
+    :param observation: reference observation for neighbours selection
+    :param y: labels for observations
+    :param variables: TODO add variables choide - require supplying variable names along with data
+    :param distance: distance function, as pairwise distances in sklearn
+    :param n: size of the sample
+    :return: selected observations and corresponding labels if provided
+    """
+    if n > data.shape[0]:
+        logging.warning("Given n ({}) is larger than data size ({})".format(n, data.shape[0]))
+        n = data.shape[0]
+    distances = distance([observation], data)[0]
+    indices = np.argpartition(distances, n - 1)[:n]
+    selected_points = data[indices, :]
+    if y is not None:
+        return selected_points, y[indices]
+    else:
+        return selected_points
--- tests/test_select.py
+++ tests/test_select.py
@@ -9,16 +9,26 @@ class TestSelect(unittest.TestCase):
 
     def setUp(self):
         self.x = np.array([[1, 0, 1], [2, 0, 2], [10, 2, 10], [1, 0, 2]])
+        self.y = np.array([11, 12.1, 13.2, 10.5])
 
     def test_select_sample(self):
         (_, m) = self.x.shape
         size = 2
-        sample = select_sample(self.x, size)
+        sample = select_sample(self.x, n=size)
         self.assertEqual(sample.shape, (size, m))
 
     def test_select_sample_2(self):
-        sample = select_sample(self.x, 1)
+        sample = select_sample(self.x, n=1)
         self.assertIn(sample[0], self.x)
+
+    def test_select_sample_3(self):
+        sample_x, sample_y = select_sample(self.x, self.y, n=1)
+        pos = list(self.y).index(sample_y[0])
+        self.assertSequenceEqual(list(sample_x[0]), list(self.x[pos]))
+
+    def test_select_sample_4(self):
+        sample_x = select_sample(self.x, n=300)
+        self.assertEqual(len(sample_x), len(self.x))
 
     def test_select_neighbours(self):
         neighbours = select_neighbours(self.x, self.x[0], n=1)
@@ -29,3 +39,12 @@ class TestSelect(unittest.TestCase):
         size = 3
         neighbours = select_neighbours(self.x, np.array([4, 3, 2]), n=size)
         self.assertEqual(neighbours.shape, (size, m))
+
+    def test_select_neighbours_3(self):
+        sample_x, sample_y = select_neighbours(self.x, np.array([4, 3, 2]), y=self.y, n=3)
+        pos = list(self.y).index(sample_y[1])
+        self.assertSequenceEqual(list(sample_x[1]), list(self.x[pos]))
+
+    def test_select_neighbours_4(self):
+        sample_x = select_neighbours(self.x, np.array([4, 3, 2]), n=300)
+        self.assertEqual(len(sample_x), len(self.x))
