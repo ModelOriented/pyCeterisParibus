667d2b0ce34310b781c2bd0f1ac13203f62839e8
Michał Kuźba <kmichael00968@gmail.com>
2019-01-09 16:58:53 +0100

    add example with multiple models

---
+++ examples/multiple_models.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sklearn import ensemble, svm
+from sklearn.linear_model import LinearRegression
+
+from ceteris_paribus.plots.plots import plot
+from ceteris_paribus.profiles import individual_variable_profile
+from ceteris_paribus.select_data import select_sample
+
+df = pd.read_csv('../datasets/insurance.csv')
+
+df = df[['age', 'bmi', 'children', 'charges']]
+
+x = df.drop(['charges'], inplace=False, axis=1)
+y = df['charges']
+var_names = list(x.columns)
+x = x.values
+y = y.values
+
+
+def linear_regression_model():
+    # Create linear regression object
+    linear_model = LinearRegression()
+
+    # Train the model using the training set
+    linear_model.fit(x, y)
+
+    # model, data, labels, variable_names
+    return linear_model, x, y, var_names
+
+
+def gradient_boosting_model():
+    gb_model = ensemble.GradientBoostingRegressor(n_estimators=1000, random_state=42)
+    gb_model.fit(x, y)
+    return gb_model, x, y, var_names
+
+
+def supported_vector_machines_model():
+    svm_model = svm.SVR(C=0.01, gamma='scale')
+    svm_model.fit(x, y)
+    return svm_model, x, y, var_names
+
+
+if __name__ == "__main__":
+    (linear_model, data, labels, variable_names) = linear_regression_model()
+    (gb_model, _, _, _) = gradient_boosting_model()
+    (svm_model, _, _, _) = supported_vector_machines_model()
+
+    cp_profile = individual_variable_profile(linear_model, data, variable_names, x[0], y[0])
+    plot(cp_profile, show_residuals=True)
+
+    sample_x, sample_y = select_sample(x, y, n=10)
+    cp2 = individual_variable_profile(gb_model, data, variable_names, sample_x, y=sample_y)
+
+    cp3 = individual_variable_profile(gb_model, data, variable_names, x[0], y[0])
+    plot(cp3, show_residuals=True)
+
+    plot(cp_profile, cp3, show_residuals=True)
