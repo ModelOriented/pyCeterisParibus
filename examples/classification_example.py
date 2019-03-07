463e95f8ea92a79b64dcde8a47a1e6f1e5a4177d
Michał Kuźba <kmichael00968@gmail.com>
2019-03-07 16:08:20 +0100

    refresh examples

--- examples/classification_example.py
+++ examples/classification_example.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 from ceteris_paribus.explainer import explain
-from ceteris_paribus.plots.plots import plot_bokeh, plot
+from ceteris_paribus.plots.plots import plot
 from ceteris_paribus.profiles import individual_variable_profile
 
 iris = load_iris()
@@ -13,6 +13,7 @@ y = iris['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
+print(iris['feature_names'])
 
 def random_forest_classifier():
     rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -27,5 +28,4 @@ if __name__ == "__main__":
     predict_function = lambda X: model.predict_proba(X)[::, 0]
     explainer_rf = explain(model, variable_names, data, labels, predict_function=predict_function)
     cp_profile = individual_variable_profile(explainer_rf, X[1], y=y[1])
-    plot_bokeh(cp_profile)
     plot(cp_profile)
