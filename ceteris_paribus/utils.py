4fe40f54c627d2dbdeb86e1c297aab631e9e8448
Michał Kuźba <kmichael00968@gmail.com>
2019-03-23 14:34:19 +0100

    remove redundant data on plots display

--- ceteris_paribus/utils.py
+++ ceteris_paribus/utils.py
@@ -49,14 +49,12 @@ def dump_observations(profiles):
     data = []
     for profile in profiles:
         for i, yhat in enumerate(profile.new_observation_predictions):
-            for var_name in profile.selected_variables:
-                d = dict(zip(profile.all_variable_names, profile.new_observation.iloc[i]))
-                d['_vname_'] = var_name
-                d['_yhat_'] = yhat
-                d['_label_'] = profile._label
-                d['_ids_'] = i
-                d['_y_'] = profile.new_observation_true[i] if profile.new_observation_true is not None else None
-                data.append(d)
+            d = dict(zip(profile.all_variable_names, profile.new_observation.iloc[i]))
+            d['_yhat_'] = yhat
+            d['_label_'] = profile._label
+            d['_ids_'] = i
+            d['_y_'] = profile.new_observation_true[i] if profile.new_observation_true is not None else None
+            data.append(d)
     return data
 
 
