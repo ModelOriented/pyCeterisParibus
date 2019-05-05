093676446c6ad5dbc9388954ffa3a6ab9e676dfd
Michał Kuźba <kmichael00968@gmail.com>
2019-05-05 21:34:58 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 PACKAGE_NAME = 'pyCeterisParibus'
 REQUIREMENTS_PATH = 'requirements.txt'
 README_PATH = 'README.md'
-VERSION = '0.5.1'
+VERSION = '0.5.2'
 
 
 def get_requirements():
